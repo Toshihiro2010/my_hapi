80d885ce4228239f30eca23eb992803d09eea181
Toshihiro2010 <s56122201044@ssru.ac.th>
2017-03-14 16:47:00 +0700

    Create Port 30 - test

--- index.js
+++ index.js
@@ -1,21 +1,40 @@
 var Hapi = require('hapi');
+var users = require('./users');
 
 var server = new Hapi.Server();
 
 server.connection({
-    host : 'localhost' , 
-    port : 1337
+    host : '0.0.0.0', 
+    port : 30
 });
 
 server.route({
     method : 'GET',
     path : '/',
     handler : function(req , res){
-        res('Hello World!');
+        res('Hello Hapi :)!');
+    }
+});
+
+server.route({
+    method : 'GET',
+    path : '/users',
+    handler : function(req , res){
+        res(users.findall());
+    }
+});
+
+server.route({
+    method : 'GET' , 
+    path : '/users/{id}',
+    handler : function(req , res){
+        var id = req.params.id;
+        res(users.findById(id));
     }
 });
 
 
+
 server.start(function(){
-    console.log("Server running at : " , server.info.uri);
+    console.log("Hapi running at : " , server.info.uri);
 });
